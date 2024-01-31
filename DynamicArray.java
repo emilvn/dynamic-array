5b46d2ae8f9afca87fe0f8eae10ab03cca7d70de
emilvn <emilvnielsen@hotmail.com>
2024-01-31 18:01:47 +0100

    changed to use grow method

--- DynamicArray.java
+++ DynamicArray.java
@@ -1,8 +1,15 @@
 public class DynamicArray {
     private Person[] arr;
+    int size;
+    final int growSize = 5;
 
-    public DynamicArray(Person[] arr){
-        this.arr = arr;
+    public DynamicArray(){
+        this.arr = new Person[0];
+    }
+
+    public DynamicArray(Person[] people){
+        this.size = people.length;
+        this.arr = people;
     }
 
     public void printArray(){
@@ -13,12 +20,11 @@ public class DynamicArray {
     }
 
     public void add(Person object){
-        Person[] newArr = new Person[size()+1];
-        for(int i = 0; i<size(); i++){
-            newArr[i] = arr[i];
+        size++;
+        if(size > arr.length){
+            grow();
         }
-        newArr[size()] = object;
-        arr = newArr;
+        arr[size-1] = object;
     }
 
     public Person get(int index){
@@ -26,26 +32,26 @@ public class DynamicArray {
     }
 
     public int size(){
-        return arr.length;
+        return size;
     }
 
     public void remove(){
-        Person[] newArr = new Person[size()-1];
-        for(int i = 0; i<size()-1; i++){
-            newArr[i] = arr[i];
-        }
-        arr = newArr;
+        size--;
+        arr[size] = null;
     }
     public void remove(int index){
-        Person[] newArr = new Person[size()-1];
-        for(int i = 0, j = 0; i<size(); i++){
+        if(index == -1){
+            return;
+        }
+        for(int i = 0, j = 0; i<size; i++){
             if(i == index){
                 continue;
             }
-            newArr[j] = arr[i];
+            arr[j] = arr[i];
             j++;
         }
-        arr = newArr;
+        size--;
+        arr[size] = null;
     }
     public void set(int index, Person object){
         arr[index] = object;
@@ -56,23 +62,24 @@ public class DynamicArray {
     }
 
     public void addAll(Person... objects){
-        int newSize = size() + objects.length;
-        Person[] newArr = new Person[newSize];
-        for(int i = 0; i<size(); i++){
-            newArr[i] = arr[i];
+        size += objects.length;
+        while(size > arr.length){
+            grow();
         }
-        for(int i = size(), j=0; i<newSize; i++, j++){
-            newArr[i] = objects[j];
+        for(int i = size-objects.length, j=0; i<size; i++, j++){
+            arr[i] = objects[j];
         }
-        arr = newArr;
     }
 
     public void insert(int index, Person object){
-        Person[] newArr = new Person[size()+1];
-        if(index == size()){
-            add(object);
-            return;
+        size++;
+        if(size < index + 1){
+            size = index+1;
         }
+        while(size > arr.length){
+            grow();
+        }
+        Person[] newArr = new Person[arr.length];
         for(int i = 0, j = 0; i<size(); i++, j++){
             if(i == index){
                 newArr[i] = object;
@@ -84,7 +91,7 @@ public class DynamicArray {
     }
 
     public int indexOf(Person object){
-        for(int i = 0; i<size(); i++){
+        for(int i = 0; i<size; i++){
             if(arr[i].equals(object)){
                 return i;
             }
@@ -94,5 +101,11 @@ public class DynamicArray {
 
     public void remove(Person object){
         remove(indexOf(object));
+    }
+
+    private void grow(){
+        Person[] newArr = new Person[size+growSize];
+        System.arraycopy(arr, 0, newArr, 0, arr.length);
+        arr = newArr;
     }
 }
--- Main.java
+++ Main.java
@@ -1,5 +1,12 @@
 public class Main {
+    DynamicArray dynamicArray;
     public static void main(String[] args) {
+        Main main = new Main();
+
+        main.test();
+    }
+
+    public void test(){
         Person person1 = new Person("Emil");
         Person person2 = new Person("Victor");
         Person person3 = new Person("Simon");
@@ -7,7 +14,55 @@ public class Main {
         Person person5 = new Person("Jonas");
         Person[] people = new Person[]{person1, person2, person3, person4, person5};
 
-        DynamicArray dynamicArray = new DynamicArray(people);
+        dynamicArray = new DynamicArray(people);
+        dynamicArray.printArray();
+        //testAdd();
+        //testRemoveLast();
+        //testRemoveIndex();
+        //testAddAll();
+        //testInsert();
+        testRemoveObject();
+    }
+    public void testAdd(){
+        System.out.println("Test add");
+        Person person = new Person("Ali");
+        dynamicArray.add(person);
+        dynamicArray.printArray();
+    }
+    public void testRemoveLast(){
+        System.out.println("Test remove last");
+        dynamicArray.remove();
+        dynamicArray.printArray();
+    }
+    public void testRemoveIndex(){
+        System.out.println("Test remove index");
+        dynamicArray.remove(3);
+        dynamicArray.printArray();
+    }
+    public void testAddAll(){
+        Person person = new Person("Ali");
+        Person person1 = new Person("H");
+        Person person2 = new Person("Tulle");
+        System.out.println("Test add all");
+        dynamicArray.addAll(person1, person2, person);
+        dynamicArray.printArray();
+    }
 
+    public void testInsert(){
+        System.out.println("Test insert");
+        Person person = new Person("Magnus");
+        dynamicArray.insert(3, person);
+        dynamicArray.printArray();
+    }
+
+    public void testRemoveObject(){
+        Person person = new Person("NewPerson");
+        System.out.println("Test remove object");
+        System.out.println("Insert new");
+        dynamicArray.insert(3, person);
+        dynamicArray.printArray();
+        System.out.println("Remove");
+        dynamicArray.remove(person);
+        dynamicArray.printArray();
     }
 }
